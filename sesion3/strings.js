c3f7226f7217372dc25242de918d6cbd224bd970
jgonzalezuam <jgonzalez@correo.uam.mx>
2022-03-09 21:03:43 -0600

    Clase 3

--- sesion3/strings.js
+++ sesion3/strings.js
@@ -12,3 +12,40 @@ console.log(MYNAME.toUpperCase());
 
 console.log(MYNAME.toUpperCase().includes(search.toUpperCase()));
 
+let weekDay  = "martes";
+
+let day = "08";
+
+let month = "Marzo";
+
+let date = weekDay + day + month;
+
+//Template String
+let datePro = `${weekDay}/${day}/${month.toLocaleUpperCase()}`;
+
+let cadenaEnVariosRenglones = `
+Esta
+Es 
+Una 
+Cadena
+De 
+Varios 
+Renglones.
+Cadena
+Otra Cadena
+`;
+
+const WORD = "Cadena";
+
+//Hace un arreglo con las partes separadas por la cadena que se pasa como argumento
+let partes = cadenaEnVariosRenglones.split(WORD);
+
+//Forma de mostrar el resultado ocupando las variables y haciendo operaciones sobre ellas como en la variable partes
+console.log(`${WORD} aparece ${partes.length-1} veces en la cadena ${cadenaEnVariosRenglones}` );
+
+//Arreglo de frutas
+let frutas = ["manzana", "pera", "sandia"];
+
+//Mostrar por consola los elementos del array frutas separado por una coma (,)
+console.log(frutas.join(","));
+
