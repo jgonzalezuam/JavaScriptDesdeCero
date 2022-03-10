f7ee670e778207f4156c4e270d2f8829c0f81bac
jgonzalezuam <jgonzalez@correo.uam.mx>
2022-03-09 19:27:50 -0600

    Primer Commit

---
+++ sesion2/variables.js
@@ -0,0 +1,25 @@
+let weekDay  = "martes";
+
+let day = "08";
+
+let month = "Marzo";
+
+let date = weekDay + day + month;
+
+let soda = 19;
+
+let sandwich = 25;
+
+let candy = 5.5;
+
+let numberOfSodas = 5
+
+let numberOfCandies = 4;
+
+let totalSodas = (numberOfSodas * soda) * (1 + IVA);
+
+let totalCandies = candy * numberOfCandies;
+
+let hasMoney = true;
+
+console.log(hasMoney);
---
+++ sesion3/strings.js
@@ -0,0 +1,14 @@
+const MYNAME = "Jorge Emmanuel González Hernández";
+
+console.log(MYNAME.length);
+
+let search = "González";
+
+console.log(MYNAME.includes(search));
+
+console.log(MYNAME.toLocaleLowerCase());
+
+console.log(MYNAME.toUpperCase());
+
+console.log(MYNAME.toUpperCase().includes(search.toUpperCase()));
+
